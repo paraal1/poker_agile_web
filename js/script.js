94a7c1375780ea32c265eb561ba5ec712c950bfd
paraal1 <alexandru.paraschivoiu25@gmail.com>
2024-12-29 13:53:50 +0200

    Added footer, style

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openGamePopUp() {
+  document.getElementById("game-popup").style.display = "block";
+}
+
+function closeGamePopUp() {
+  document.getElementById("game-popup").style.display = "none";
+}
